fbab21b14dbdc155f9cff249778759a7d50d1188
SaiVyshnavi08 <saivyshnavigudipalli@gmail.com>
2022-12-24 21:50:49 +0530

    done

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,23 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword == this.password){
+            if(newPassword.length() >= 8  ){
+               int uppercase =0;
+               int lowercase =0;
+               int digit =0;
+               int special =0;
+                for(int i=0 ; i<newPassword.length() ; i++){
+                    char ch = newPassword.charAt(i);
+                    if(ch >= 'A' && ch<= 'Z' ) uppercase++;
+                    else if(ch >='a' && ch<='z') lowercase++;
+                    else if(ch >='0' && ch<= '9') digit++;
+                    else special++;
+                }
+                if(uppercase>=1 && lowercase>=1 && digit >=1 && special>=1){
+                    this.password = newPassword;
+                }
+            }
+        }
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -12,12 +12,15 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
+    super(emailId);
+    int  inboxCapacity = Integer.MAX_VALUE;
+    calendar = new ArrayList<>();
 
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+   calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +28,17 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+        Collections.sort(calendar ,(Meeting m1 , Meeting m2) -> m1.getEndTime().compareTo(m2.getEndTime()));
+        Meeting m2  = calendar.get(0);
+        LocalTime currentEndTime = LocalTime.of(00,00,00);
+        int maxMeetings =0;
+        for(int i=0 ;i<calendar.size(); i++){
+            Meeting CurrentMeeting = calendar.get(i);
+            if(CurrentMeeting.getStartTime().compareTo(currentEndTime)>0) {
+                maxMeetings++;
+                currentEndTime = CurrentMeeting.getEndTime();
+            }
+        }
+        return maxMeetings;
     }
 }
